f7df2190f86d0047cac52127007771524e414ca5
DimitarGeorgievski <muzipuzi123@gmail.com>
2025-05-06 15:26:50 +0200

    fixed navbar and some bugs

--- src/script.js
+++ src/script.js
@@ -5,5 +5,6 @@ menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("opacity-0");
   mobileMenu.classList.toggle("invisible");
   mobileMenu.classList.toggle("visible");
+  mobileMenu.classList.toggle("mb-96")
 });
 
