bc94cd3213287b16e0f3ce21fa668114f08f12b8
MarijaBogeska <kk.marijabogeska@gmail.com>
2025-06-21 16:20:24 +0200

    changed png to url

--- lecturers/script.js
+++ lecturers/script.js
@@ -1,5 +1,4 @@
 import LecturersService from "../src/services/lecturers.service.js";
-history.pushState({}, "", "/lecturers");
 
 // Navbar elements
 const menuToggle = document.getElementById("menuToggle");
@@ -61,11 +60,7 @@ let lecturersCards = (lecturers) => {
       const lecturerId = e.currentTarget.id;
       const foundLecturer = LecturersService.getById(lecturerId);
       if (foundLecturer) {
-        history.pushState(
-          { id: lecturerId },
-          null,
-          `/lecturers/?id=${lecturerId}`
-        );
+        history.pushState({ id: lecturerId }, null, `?id=${lecturerId}`);
         profilesSection.style.display = "flex";
         lecturersHomeSection.style.display = "none";
         window.scrollTo({ top: 0, behavior: "smooth" });
@@ -80,9 +75,9 @@ let lecturersCards = (lecturers) => {
 function getLecturerProfile(lecturer) {
   let lecturerHTML = `<article
         id="profile${lecturer.id}" "
-        class="flex flex-col md:flex-row justify-center gap-5 m-20 md:m-20 -mt-64"
+        class="flex flex-col md:flex-row justify-center gap-5 m-8 md:m-20 -mt-64"
       >
-        <section class="md:absolute md:left-6 lg:left-32 justify-items-center">
+        <section class="md:absolute md:left-8 lg:left-32 justify-items-center">
           <img
             src="${lecturer.photoUrl}"
             class="md:w-72 md:h-72 w-40 h-40 md:mx-auto object-cover rounded-full border-4 border-[${
@@ -102,7 +97,7 @@ function getLecturerProfile(lecturer) {
             <div class="grid justify-items-start gap-2 mt-4 justify-center">
               <div class="flex flex-row justify-center gap-2">
                 <img
-                  src="../src/img/mail (1).png"
+                  src="https://raw.githubusercontent.com/qa-codecademy/sp2025-cp01-lbf-1/refs/heads/main/src/img/mail%20(1).png"
                   alt="mail"
                   class="md:w-6 md:h-6 w-4 h-4"
                 />
@@ -110,7 +105,7 @@ function getLecturerProfile(lecturer) {
               </div>
               <div class="flex flex-row justify-center gap-2">
                 <img
-                  src="../src/img/linkedin.png"
+                  src="https://raw.githubusercontent.com/qa-codecademy/sp2025-cp01-lbf-1/refs/heads/main/src/img/linkedin.png"
                   alt="linkedin"
                   class="md:w-6 md:h-6 w-4 h-4"
                 />
@@ -122,11 +117,10 @@ function getLecturerProfile(lecturer) {
                   >
                 </p>
               </div>
-            </div>
           </div>
           <nav class="my-10 flex justify-center text-[${
             colors[Number(lecturer.id) - 1]
-          }]">
+          }] text-left">
             <ul>
               <li
                 class="pb-3 hover:scale-105 transition transform duration-300"
@@ -165,18 +159,20 @@ function getLecturerProfile(lecturer) {
           <!-- БИОГРАФИЈА -->
           <section
             id="biography${lecturer.id}"
-            class="w-full max-w-[800px] rounded-3xl bg-white scroll-mt-20 lg:scroll-mt-32 mx-10"
+            class="w-full max-w-[800px] min-w-[200px] rounded-3xl bg-white scroll-mt-20 lg:scroll-mt-32 mx-auto"
           >
-            <div class="flex flex-row m-2 flex-wrap">
+            <div class="flex flex-row m-4 flex-wrap">
               <img
-                src="../src/img/writer${lecturer.id}.png"
+                src="https://raw.githubusercontent.com/qa-codecademy/sp2025-cp01-lbf-1/refs/heads/main/src/img/writer${
+                  lecturer.id
+                }.png"
                 alt="writer"
-                class="md:w-10 md:h-10 mt-1 w-4 h-4 relative left-5"
+                class="md:w-10 md:h-10 mt-1 w-8 h-8 relative left-5"
               />
               <h1
-                class="relative left-5 ml-4 md:mt-2 text-[${
+                class="relative left-5 ml-4 mt-2 text-[${
                   colors[Number(lecturer.id) - 1]
-                }] md:text-2xl text-l align-center"
+                }] text-lg md:text-xl lg:text-2xl align-center"
               >
                 БИОГРАФИЈА
               </h1>
@@ -189,18 +185,20 @@ function getLecturerProfile(lecturer) {
           <!-- ОБРАЗОВАНИЕ -->
           <section
             id="education${lecturer.id}"
-            class="w-full max-w-[800px] rounded-3xl bg-white scroll-mt-20 gap-10 lg:scroll-mt-32 mx-10"
+            class="w-full max-w-[800px] min-w-[200px] rounded-3xl bg-white scroll-mt-20 gap-10 lg:scroll-mt-32 mx-auto"
           >
-            <div class="flex flex-row m-2 flex-wrap">
+            <div class="flex flex-row m-4 flex-wrap">
               <img
-                src="../src/img/mortarboard${lecturer.id}.png"
+                src="https://raw.githubusercontent.com/qa-codecademy/sp2025-cp01-lbf-1/refs/heads/main/src/img/mortarboard${
+                  lecturer.id
+                }.png"
                 alt="mortarboard"
-                class="md:w-10 md:h-10 mt-1 w-4 h-4 relative left-5"
+                class="md:w-10 md:h-10 mt-1 w-8 h-8 relative left-5"
               />
               <h1
-                class="relative left-5 ml-4 md:mt-2 text-[${
+                class="relative left-5 ml-4 mt-2 text-[${
                   colors[Number(lecturer.id) - 1]
-                }] md:text-2xl text-l align-center"
+                }] text-lg md:text-xl lg:text-2xl align-center"
               >
                 ОБРАЗОВАНИЕ
               </h1>
@@ -223,18 +221,20 @@ function getLecturerProfile(lecturer) {
           <!-- КАРИЕРА -->
           <section
             id="career${lecturer.id}"
-            class="w-full max-w-[800px] rounded-3xl bg-white scroll-mt-20 lg:scroll-mt-32 mx-10"
+            class="w-full max-w-[800px] min-w-[200px] rounded-3xl bg-white scroll-mt-20 lg:scroll-mt-32 mx-auto"
           >
-            <div class="flex flex-row m-2 flex-wrap">
+            <div class="flex flex-row m-4 flex-wrap">
               <img
-                src="../src/img/career${lecturer.id}.png"
+                src="https://raw.githubusercontent.com/qa-codecademy/sp2025-cp01-lbf-1/refs/heads/main/src/img/career${
+                  lecturer.id
+                }.png"
                 alt="career"
-                class="md:w-10 md:h-10 mt-1 w-4 h-4 relative left-5"
+                class="md:w-10 md:h-10 mt-1 w-8 h-8 relative left-5"
               />
               <h1
-                class="relative left-5 ml-4 md:mt-2 text-[${
+                class="relative left-5 ml-4 mt-2 text-[${
                   colors[Number(lecturer.id) - 1]
-                }] md:text-2xl text-l align-center"
+                }] text-lg md:text-xl lg:text-2xl align-center"
               >
                 КАРИЕРА
               </h1>
@@ -256,18 +256,20 @@ function getLecturerProfile(lecturer) {
           <!-- ПРОГРАМИ -->
           <section
             id="programs${lecturer.id}"
-            class="w-full max-w-[800px] rounded-3xl bg-white scroll-mt-20 lg:scroll-mt-32 mx-10"
+            class="w-full max-w-[800px] min-w-[200px] rounded-3xl bg-white scroll-mt-20 lg:scroll-mt-32 mx-auto"
           >
-            <div class="flex flex-row m-2 flex-wrap">
+            <div class="flex flex-row m-4 flex-wrap">
               <img
-                src="../src/img/notebook${lecturer.id}.png"
+                src="https://raw.githubusercontent.com/qa-codecademy/sp2025-cp01-lbf-1/refs/heads/main/src/img/notebook${
+                  lecturer.id
+                }.png"
                 alt="notebook"
-                class="md:w-10 md:h-10 mt-1 w-4 h-4 relative left-5"
+                class="md:w-10 md:h-10 mt-1 w-8 h-8 relative left-5"
               />
               <h1
-                class="relative left-5 ml-4 md:mt-2 text-[${
+                class="relative left-5 ml-4 mt-2 text-[${
                   colors[Number(lecturer.id) - 1]
-                }] md:text-2xl text-l align-center"
+                }] text-lg md:text-xl lg:text-2xl align-center"
               >
                 ПРОГРАМИ
               </h1>
@@ -329,6 +331,22 @@ nextBtn.addEventListener("click", () => {
 });
 
 // EVENTS
+// Run this on page load
+window.addEventListener("DOMContentLoaded", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const idFromUrl = urlParams.get("id");
+
+  if (idFromUrl) {
+    const lecturer = LecturersService.getById(idFromUrl);
+    if (lecturer) {
+      profilesSection.style.display = "flex";
+      lecturersHomeSection.style.display = "none";
+      getLecturerProfile(lecturer);
+    }
+  }
+});
+
+// Handle back/forward navigation
 window.addEventListener("popstate", () => {
   const urlParams = new URLSearchParams(window.location.search);
   const idFromUrl = urlParams.get("id");
