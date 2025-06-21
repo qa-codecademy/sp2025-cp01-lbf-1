b15852b456f7661a455133f93a765861c9104a8c
MarijaBogeska <kk.marijabogeska@gmail.com>
2025-06-21 16:50:40 +0200

    changes on responsive home page

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,7 +5,7 @@ menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("opacity-0");
   mobileMenu.classList.toggle("invisible");
   mobileMenu.classList.toggle("visible");
-  mobileMenu.classList.toggle("mb-72");
+  mobileMenu.classList.toggle("mb-96");
 });
 import ProgramService from "../services/program.service.js";
 
