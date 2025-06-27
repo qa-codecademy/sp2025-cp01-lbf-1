a90f6742971fe79062ec7902400f1ee44113854a
unknown <milan.ognjanoski@gmail.com>
2025-06-27 13:30:03 +0200

    Fixed contact page responsiveness, minor UI fixes, added for validation

--- contact/script.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.getElementById("menuToggle");
-const mobileMenu = document.getElementById("mobileMenu");
-
-menuToggle.addEventListener("click", () => {
-  mobileMenu.classList.toggle("opacity-0");
-  mobileMenu.classList.toggle("invisible");
-  mobileMenu.classList.toggle("visible");
-  mobileMenu.classList.toggle("mb-96");
-});
